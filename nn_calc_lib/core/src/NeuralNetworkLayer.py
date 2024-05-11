9f94ff15fc533a9916c77d4b07499660b2ed33e7
caleb7023 <caleb70230@gmail.com>
2024-05-11 14:46:50 +0900

    Added is_input_layer parameter to NeuralNetworkLayer constructor, _value now calcs automatically

--- nn_calc_lib/core/src/NeuralNetworkLayer.py
+++ nn_calc_lib/core/src/NeuralNetworkLayer.py
@@ -6,7 +6,7 @@ import core.src.Neuron as Neuron
 
 import numpy as np
 
-def __init__(self, neurons:list=None, *, input_size:int=None, bias_random_range:tuple=None, weights_random_range:tuple=None, neuron_size:int=None,activation_function)->None:
+def __init__(self, neurons:list=None, *, input_size:int=None, bias_random_range:tuple=None, weights_random_range:tuple=None, neuron_size:int=None, activation_function=None, is_input_layer:bool=False)->None:
 
     if neurons is None:
         if input_size is None:
@@ -14,9 +14,17 @@ def __init__(self, neurons:list=None, *, input_size:int=None, bias_random_range:
         elif input_size < 1:
             raise ValueError("input_size must be greater than 0")
         else:
-            self._neurons = [Neuron(neuron_bias=None, neuron_weights=None, input_size=input_size, activation_function=activation_function) for i in range(neuron_size)]
-        
-    self._value = None
+            self._neurons = [Neuron(neuron_bias=None,
+                                    neuron_weights=None,
+                                    input_size=input_size,
+                                    bias_random_range=bias_random_range,
+                                    weights_random_range=weights_random_range,
+                                    activation_function=activation_function
+            ) for i in range(neuron_size)]
+            self._value = np.empty(neuron_size, dtype=float)
+    else:
+        self._neurons = neurons
+        self._value = np.empty(len(neurons), dtype=float)
 
 def __mul__(self, other:np.ndarray)->np.ndarray:
     if other.ndim != 1:
