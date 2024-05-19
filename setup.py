4a6af09c45fb732bddd516587007b76a91d76b29
caleb7023 <caleb70230@gmail.com>
2024-05-20 06:27:38 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/user/bin/env
+
+# author : caleb7023
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nn_calc_lib",
+    version="1.0.0",
+    packages=find_packages(),
+    author="caleb7023",
+    description="A lib for neural network calculation",
+    install_requires=[
+        "numpy",
+    ],
+)
