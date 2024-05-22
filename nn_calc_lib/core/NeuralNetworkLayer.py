a06700801628464e6299e906cfd26183ad68a9bf
caleb7023 <caleb70230@gmail.com>
2024-05-22 21:02:15 +0900

    removed Neuron.py and now the nn calcs without it

--- nn_calc_lib/core/NeuralNetworkLayer.py
+++ nn_calc_lib/core/NeuralNetworkLayer.py
@@ -4,60 +4,60 @@
 
 import numpy as np
 
-from .Neuron import Neuron
-
 class NeuralNetworkLayer:
 
-    def __init__(self, neurons:list=None, *, input_size:int=None, bias_random_range:tuple=(-1, 1), weights_random_range:tuple=(-1, 1), neuron_size:int=None, activation_function=None, derivative_function=None, is_input_layer:bool=False)->None:
+    def __init__(self, neuron_weights:np.ndarray=None, *, neuron_bias:np.ndarray=None, input_size:int=None, bias_random_range:tuple=(-1, 1), weights_random_range:tuple=(-1, 1), neuron_size:int=None, activation_function=None, derivative_function=None, is_input_layer:bool=False)->None:
         """
         
         ### Parameters
 
-        neurons             : List of Neuron objects. For custom neural network.
+        ```plaintext
+        neuron_weights      : The weights of the hidden layer's neurons.
+        neuron_bias         : The bias of the hidden layer's neurons. Must be provided if neuron_weights is provided.
         input_size          : The size of the input layer's neurons. Any dimension can be provided if it is a input layer.
-        bias_random_range   : The random range of the hidden layer's neurons bias. Will be ignored if neurons is provided. If not provided, (-1, 1) will be used.
-        weights_random_range: The random range of the hidden layer's neurons weights. Will be ignored if neurons is provided. If not provided, (-1, 1) will be used.
-        neuron_size         : The size of the hidden layer's neurons. Only single dimension can be provided. If is_input_layer is True, Any dimension can be allowed but it is gonna be converted into a single dimension. Will be ignored if neurons is provided.
-        activation_function : The activation function of the hidden layer's neurons. Will be ignored if neurons is provided.
-        derivative_function : The derivative of the activation function. Will be ignored if neurons or activation_function is provided. Recommended to provide.
+        bias_random_range   : The random range of the hidden layer's neuron's bias. Will be ignored if neuron_weights and neuron_bias is provided. If not provided, (-1, 1) will be used.
+        weights_random_range: The random range of the hidden layer's neuron's weights. Will be ignored if neurons is provided. If not provided, (-1, 1) will be used.
+        neuron_size         : The size of the hidden layer's neurons. Only single dimension can be provided. Will be ignored if neuron_weights and neuron_bias is provided.
+        activation_function : The activation function of the hidden layer's neurons. Will be ignored if neuron_weights and neuron_bias is provided.
+        derivative_function : The derivative of the activation function. Recommended to provide.
         is_input_layer      : Is the layer for input or not. If this was set to True, parameters other than neuron_size will be ignored.
-        
+        ```
+
         """
+
+        self.neurons_weights:np.ndarray # Shape: (input_size, neuron_size)
+        self.neurons_biases :np.ndarray # Shape: (neuron_size)
 
         if is_input_layer:
             if neuron_size is None:
                 raise ValueError("neuron_size must be provided if is_input_layer is True")
-            self.value = np.zeros(neuron_size, dtype=float)
         else:
-            if neurons is None:
+            if neuron_weights is None and neuron_bias is None:
                 if input_size is None:
                     raise ValueError("input_size must be provided if neurons is not provided")
                 elif input_size < 1:
                     raise ValueError("input_size must be greater than 0")
-                else:
-                    self._neurons = [Neuron(neuron_bias=None,
-                                            neuron_weights=None,
-                                            input_size=input_size,
-                                            bias_random_range=bias_random_range,
-                                            weights_random_range=weights_random_range,
-                                            activation_function=activation_function,
-                                            derivative_function=derivative_function
-                    ) for i in range(neuron_size)]
-                    self.value = np.empty(neuron_size, dtype=float)
+                # Create neurons for the layer
+                self.neurons_weights = np.random.uniform(weights_random_range[0], weights_random_range[1], (input_size, neuron_size))
+                self.neurons_biases  = np.random.uniform(bias_random_range   [0], bias_random_range   [1],              neuron_size )
             else:
-                self._neurons = neurons
-                self.value = np.empty(len(neurons), dtype=float)
-        self._is_input_layer = is_input_layer
+                self.neurons_weights = neuron_weights
+                self.neurons_biases  = neuron_bias
+            self.activation_function = activation_function
+            self.derivative_function = derivative_function
+        self.value = None
+        self.is_input_layer = is_input_layer
 
     def forward_propagation(self, input_data:np.ndarray)->np.ndarray:
         if input_data.ndim != 1:
             raise ValueError("input_data must be a 1D array")
-        for neuron in self._neurons: # forward propagation for each neuron in the layer
-            neuron.forward_propagation(input_data)
-        self.value = np.array([neuron.value for neuron in self._neurons])
+        # Forward propagation for the layer
+        self.last_input = input_data
+        self.actfunc_input = (self.neurons_weights * input_data[:, None]).sum(axis=0) + self.neurons_biases
+        self.value = self.activation_function(self.actfunc_input)
 
     def backword_propagation(self, losses:np.ndarray, learning_rate:float=0.01)->np.ndarray:
-        if self._is_input_layer:
+        if self.is_input_layer:
             return None
         
         # Checking parameters for error
@@ -67,14 +67,33 @@ class NeuralNetworkLayer:
             raise ValueError("losses must be a 1D array")
         if len(losses) != len(self.value):
             raise ValueError("losses must have the same length as the size of the neural network layer")
-        
+
         # Backward propagation
-        # Setting the next layer's losses
-        next_layer_losses = np.zeros(len(self._neurons[0]._neuron_weights), dtype=float)
+        if self.derivative_function is not None:
+            derivative = self.derivative_function(self.actfunc_input, self.value)
+        else:
+            derivative = __derivative(self.actfunc_input, self.value, self.activation_function)
 
-        # Calulating the next layer's losses and updating the weights and biases for each neuron
-        for neuron, loss in zip(self._neurons, losses):
-            next_layer_losses += neuron.backward_propagation(loss, learning_rate)
+        gradient = derivative * losses * learning_rate # Shape: (neuron_size)
+        # Calculate the losses for the previous layer
+        layer_losses = (gradient[None, :] * self.neurons_weights).sum(axis=1)
+        # Update the weights and biases
+        self.neurons_weights -= self.last_input[:, None] * gradient
+        self.neurons_biases  -= gradient
+        # Return the losses for the previous layer
+        return layer_losses
 
-        # Returning the next layer's losses
-        return next_layer_losses
+
+def __derivative(x:np.ndarray, y:np.ndarray, activation_function)->np.ndarray:
+    MAX_ERROR = 0.0001
+    error = 1
+    activation_function_x = activation_function(x)
+    derivative_result = ((activation_function(x+0.01)-activation_function_x))*100
+    dt = 0.005
+    dt_recip = 200
+    while MAX_ERROR < error:
+        last_derivative_result = derivative_result
+        derivative_result = (activation_function(x+dt)-activation_function_x)*dt_recip
+        error = abs(derivative_result-last_derivative_result)
+        dt_recip *= 2
+        dt *= 0.5
